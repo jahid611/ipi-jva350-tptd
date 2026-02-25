f7012912a5d9e61fc3646c2bf7e7cb68622ca49f
jahid611 <jahidsayad42@gmail.com>
2026-02-25 10:01:35 +0100

    fix: ajout de la methode paques manquante

--- src/main/java/com/ipi/jva350/model/Entreprise.java
+++ src/main/java/com/ipi/jva350/model/Entreprise.java
@@ -129,6 +129,25 @@ public final class Entreprise {
                 : LocalDate.of(d.getYear() - 1, 6, 1);
     }
 
+    // algorithme pour calculer la date de paques
+    public static LocalDate paques(int annee) {
+        int a = annee % 19;
+        int b = annee / 100;
+        int c = annee % 100;
+        int d = b / 4;
+        int e = b % 4;
+        int f = (b + 8) / 25;
+        int g = (b - f + 1) / 3;
+        int h = (19 * a + b - d - g + 15) % 30;
+        int i = c / 4;
+        int k = c % 4;
+        int l = (32 + 2 * e + 2 * i - h - k) % 7;
+        int m = (a + 11 * h + 22 * l) / 451;
+        int mois = (h + l - 7 * m + 114) / 31;
+        int jour = ((h + l - 7 * m + 114) % 31) + 1;
+        return LocalDate.of(annee, mois, jour);
+    }
+
 public static boolean estJourFerie(LocalDate jour) {
         int mois = jour.getMonthValue();
         int quantieme = jour.getDayOfMonth();
