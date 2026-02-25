dcf939fe24ce1e65772e5abf1f7fb33c832409d4
jahid611 <jahidsayad42@gmail.com>
2026-02-25 13:43:15 +0100

    test: add unit test for SalarieAideADomicile constructor

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -41,4 +41,14 @@ public class SalarieAideADomicileTest {
         
         assertEquals(attendu, resultat);
     }
+
+    @Test
+void testSalarieComplet() {
+    SalarieAideADomicile s = new SalarieAideADomicile("Jean", 
+        LocalDate.now(), LocalDate.now(), 10.0, 25.0, 15.0, 2.0, 0.0);
+    
+    assertEquals("Jean", s.getNom());
+    assertNotNull(s.getMoisDebutContrat());
+    assertTrue(s.getCongesPayesAcquisAnneeN() > 0);
+}
 }
